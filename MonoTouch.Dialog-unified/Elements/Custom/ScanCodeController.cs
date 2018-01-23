2bff3ac8cd55353747a97be5ffd96cb337e4afcc
iOS Dev Cluster <iosdevcluster@san16970.replynet.prv>
2018-01-23 11:54:42 +0100

    Scanner
    Add Scanner

---
+++ MonoTouch.Dialog-unified/Elements/Custom/ScanCodeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using UIKit;
+using ZXing.Mobile;
+
+namespace MonoTouch.Dialog
+{
+    public partial class ScanCodeController : UIViewController
+    {
+        public ScanCodeController() : base("ScanCodeController", null)
+        {
+        }
+
+        public async override void ViewDidLoad()
+        {
+            MobileBarcodeScanner _scanner = new MobileBarcodeScanner();
+          
+            var result = await _scanner.Scan();
+
+            if (result != null)
+            {
+                OnSendResponse(result.Text);
+                BeginInvokeOnMainThread(() => NavigationController.PopViewController(true));
+            }
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+        public event EventHandler<StringEventArgs> SendResponse;
+        private void OnSendResponse(string scanResult)
+        {
+            if (SendResponse != null)
+            {
+                SendResponse(this, new StringEventArgs { ScannerResult = scanResult });
+            }
+        }
+        public override void ViewDidDisappear(bool animated)
+        {
+            BeginInvokeOnMainThread(() => NavigationController.PopViewController(true));
+            base.ViewDidDisappear(animated);
+        }
+        public class StringEventArgs : EventArgs
+        {
+
+            public string ScannerResult { get; set; }
+
+        }
+    }
+}
+
---
+++ MonoTouch.Dialog-unified/Elements/Custom/ScanCodeElement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace MonoTouch.Dialog
+{
+    public class ScanCodeElement : LoadMoreElement
+    {
+
+
+        public ScanCodeElement(string caption) : base(caption,string.Empty,null)
+        {
+            
+        }
+
+
+        public override void Selected(DialogViewController dvc, UIKit.UITableView tableView, Foundation.NSIndexPath indexPath)
+        {
+            base.Selected(dvc, tableView, indexPath);
+            var scan = new ScanCodeController()
+            {
+
+            };
+            this.Animating = false;
+
+            scan.SendResponse += (s, e) =>
+            {
+                OnSendResponse(e.ScannerResult);
+            };
+            dvc.ActivateController(scan);
+        }
+
+
+
+        public override UIKit.UITableViewCell GetCell(UIKit.UITableView tv)
+        {
+            return base.GetCell(tv);
+        }
+
+        public event EventHandler<StringEventArgs> SendResponse;
+        private void OnSendResponse(string scanResult)
+        {
+            if (SendResponse != null)
+            {
+                SendResponse(this, new StringEventArgs{ScannerResult=scanResult});
+            }
+        }
+
+        public class StringEventArgs : EventArgs
+        {
+
+            public string ScannerResult { get; set; }
+
+        }
+
+    }
+}
