5ae4228759864beb4179363a06c39d55162359ac
iOS Dev Cluster <iosdevcluster@m-bernardini2.replynet.prv>
2018-02-27 09:40:44 +0100

    boh
    boh

--- MonoTouch.Dialog-unified/Elements/Custom/PhotoViewController.cs
+++ MonoTouch.Dialog-unified/Elements/Custom/PhotoViewController.cs
@@ -4,148 +4,167 @@ using UIKit;
 
 namespace MonoTouch.Dialog
 {
-	public partial class PhotoViewController : UIViewController
-	{
-		UIBarButtonItem btnClear;
-		private string _BtnSelectPhoto;
-		private string _BtnTakePicture;
-		private string _deleteButtonLabel;
-		
-
-		public PhotoViewController(string BtnSelectPhoto, string BtnTakePicture,string DeleteButtonLabel) : base("PhotoViewController", null)
-		{
-			this._BtnSelectPhoto = BtnSelectPhoto;
-			this._BtnTakePicture = BtnTakePicture;
-			this._deleteButtonLabel = DeleteButtonLabel;
-
-		}
+    public partial class PhotoViewController : UIViewController
+    {
+        UIBarButtonItem btnClear;
+        private string _BtnSelectPhoto;
+        private string _BtnTakePicture;
+        private string _deleteButtonLabel;
 
 
-		public override void ViewDidLoad()
-		{
-			base.ViewDidLoad();
+        public PhotoViewController(string BtnSelectPhoto, string BtnTakePicture, string DeleteButtonLabel) : base("PhotoViewController", null)
+        {
+            this._BtnSelectPhoto = BtnSelectPhoto;
+            this._BtnTakePicture = BtnTakePicture;
+            this._deleteButtonLabel = DeleteButtonLabel;
 
-			btnClear = new UIBarButtonItem(_deleteButtonLabel, UIBarButtonItemStyle.Done,(object sender, EventArgs e) =>
-				   {
-					   OnSendResponse(null);
-					   BeginInvokeOnMainThread(() => NavigationController.PopViewController(true));
-				
-				   });
-			this.NavigationItem.SetRightBarButtonItem(btnClear,false);
-		
-			SelectPhotoBtn.SetTitle(_BtnSelectPhoto, UIControlState.Normal);
+        }
+        private UINavigationController FindNavigationController(UIViewController viewController)
+        {
+            if (viewController != null)
+            {
+                if (viewController.NavigationController != null)
+                    return viewController.NavigationController;
+                foreach (var child in viewController.ChildViewControllers)
+                {
+                    var nc = FindNavigationController(child);
+                    if (nc != null)
+                        return nc;
+                }
+            }
+            return null;
+        }
 
-			SelectPhotoBtn.TouchUpInside += (sender, e) =>
-			  {
-				  Camera.SelectPicture(this, (obj) =>
-				  {
-					  var photo = obj.ValueForKey(new NSString("UIImagePickerControllerOriginalImage")) as UIImage;
-					  OnSendResponse(photo);
-					  BeginInvokeOnMainThread(() => NavigationController.PopViewController(true));
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
 
-				  });
-			  };
+            btnClear = new UIBarButtonItem(_deleteButtonLabel, UIBarButtonItemStyle.Done, (object sender, EventArgs e) =>
+                    {
+                        OnSendResponse(null);
+                        BeginInvokeOnMainThread(() => NavigationController.PopViewController(true));
 
-			takePicture.SetTitle(_BtnTakePicture, UIControlState.Normal);
-			takePicture.TouchUpInside += (sender, e) =>
-			 {
-				 TakePhoto(this);
-			 };
+                    });
+            this.NavigationItem.SetRightBarButtonItem(btnClear, false);
 
-			//NavigationItem.RightBarButtonItem = new UIBarButtonItem();
+            SelectPhotoBtn.SetTitle(_BtnSelectPhoto, UIControlState.Normal);
 
-			// Perform any additional setup after loading the view, typically from a nib.
-		}
+            SelectPhotoBtn.TouchUpInside += (sender, e) =>
+              {
+                  Camera.SelectPicture(this, (obj) =>
+                  {
+                      var photo = obj.ValueForKey(new NSString("UIImagePickerControllerOriginalImage")) as UIImage;
+                      OnSendResponse(photo);
+                      BeginInvokeOnMainThread(() =>
+                        {
+                            var nav = NavigationController;
+                            //if (nav == null)
+                                //nav = FindNavigationController(UIApplication.SharedApplication.KeyWindow.RootViewController);
+                            nav.PopViewController(true);
+                        });
+                  }); 
+              };
 
-		public override void DidReceiveMemoryWarning()
-		{
-			base.DidReceiveMemoryWarning();
-			// Release any cached data, images, etc that aren't in use.
-		}
+            takePicture.SetTitle(_BtnTakePicture, UIControlState.Normal);
+            takePicture.TouchUpInside += (sender, e) =>
+             {
+                 TakePhoto(this);
+             };
 
-		public event EventHandler<CapturePhotoEventArgs> SendResponse;
-		private void OnSendResponse(UIImage image)
-		{
-			//if (SendResponse != null)
-			//{
-			SendResponse(this, new CapturePhotoEventArgs { Value = image });
-			//}
-		}
+            //NavigationItem.RightBarButtonItem = new UIBarButtonItem();
 
-		public class CapturePhotoEventArgs : EventArgs
-		{
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
 
-			public UIImage Value { get; set; }
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
 
-		}
+        public event EventHandler<CapturePhotoEventArgs> SendResponse;
+        private void OnSendResponse(UIImage image)
+        {
+            //if (SendResponse != null)
+            //{
+            SendResponse(this, new CapturePhotoEventArgs { Value = image });
+            //}
+        }
 
-		private void TakePhoto(UIViewController uvc)
-		{
-			Camera.TakePicture(uvc, (obj) =>
-			{
-				var photo = obj.ValueForKey(new NSString("UIImagePickerControllerOriginalImage")) as UIImage;
-		//Value = photo;
-		//Value = photo.Scale(new CGSize(this.newHeight * photo.Size.Width / photo.Size.Height, this.newHeight));
-		OnSendResponse(photo);
-				BeginInvokeOnMainThread(() => NavigationController.PopViewController(true));
-			});
-		}
-	}
+        public class CapturePhotoEventArgs : EventArgs
+        {
 
+            public UIImage Value { get; set; }
 
+        }
 
-	public static class Camera
-	{
-		static UIImagePickerController picker;
-		static Action<NSDictionary> _callback;
-		public static UIViewController _parent = null;
-
-		static void Init()
-		{
-			if (picker != null)
-				return;
-
-			picker = new UIImagePickerController();
-			picker.Delegate = new CameraDelegate();
-		}
-
-		class CameraDelegate : UIImagePickerControllerDelegate
-		{
-			public override void FinishedPickingMedia(UIImagePickerController picker, NSDictionary info)
-			{
-				var cb = _callback;
-				_callback = null;
-				picker.DismissViewController(true, null);
-				cb(info);
-			}
-		}
-
-		public static void TakePicture(UIViewController parent, Action<NSDictionary> callback)
-		{
-			Init();
-			_parent = parent;
-			picker.SourceType = UIImagePickerControllerSourceType.Camera;
-			_callback = callback;
-			//parent.PresentModalViewController (picker, true);
-			//parent.NavigationController.PushViewController (picker, true);
-			//((DialogViewController)parent).ActivateController(picker);
-			parent.PresentViewController(picker, true, null);
-
-
-		}
-
-		public static void SelectPicture(UIViewController parent, Action<NSDictionary> callback)
-		{
-			Init();
-			_parent = parent;
-			picker.SourceType = UIImagePickerControllerSourceType.PhotoLibrary;
-			_callback = callback;
-			//((DialogViewController)parent).ActivateController(picker);
-			parent.PresentViewController(picker, true, null);
-		}
+        private void TakePhoto(UIViewController uvc)
+        {
+            Camera.TakePicture(uvc, (obj) =>
+            {
+                var photo = obj.ValueForKey(new NSString("UIImagePickerControllerOriginalImage")) as UIImage;
+                //Value = photo;
+                //Value = photo.Scale(new CGSize(this.newHeight * photo.Size.Width / photo.Size.Height, this.newHeight));
+                OnSendResponse(photo);
+                BeginInvokeOnMainThread(() => NavigationController.PopViewController(true));
+            });
+        }
+    }
 
 
 
-	}
+    public static class Camera
+    {
+        static UIImagePickerController picker;
+        static Action<NSDictionary> _callback;
+        public static UIViewController _parent = null;
+
+        static void Init()
+        {
+            if (picker != null)
+                return;
+
+            picker = new UIImagePickerController();
+            picker.Delegate = new CameraDelegate();
+        }
+
+        class CameraDelegate : UIImagePickerControllerDelegate
+        {
+            public override void FinishedPickingMedia(UIImagePickerController picker, NSDictionary info)
+            {
+                var cb = _callback;
+                _callback = null;
+                picker.DismissViewController(true, null);
+                cb(info);
+            }
+        }
+
+        public static void TakePicture(UIViewController parent, Action<NSDictionary> callback)
+        {
+            Init();
+            _parent = parent;
+            picker.SourceType = UIImagePickerControllerSourceType.Camera;
+            _callback = callback;
+            //parent.PresentModalViewController (picker, true);
+            //parent.NavigationController.PushViewController (picker, true);
+            //((DialogViewController)parent).ActivateController(picker);
+            parent.PresentViewController(picker, true, null);
+
+
+        }
+
+        public static void SelectPicture(UIViewController parent, Action<NSDictionary> callback)
+        {
+            Init();
+            _parent = parent;
+            picker.SourceType = UIImagePickerControllerSourceType.PhotoLibrary;
+            _callback = callback;
+            //((DialogViewController)parent).ActivateController(picker);
+            parent.PresentViewController(picker, true, null);
+        }
+
+
+
+    }
 }
 
